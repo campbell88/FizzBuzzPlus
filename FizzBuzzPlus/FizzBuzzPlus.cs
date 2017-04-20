91ef1597fd80f9e1b6e7d5564251b3b860c6367f
campbell88 <shatharcampbell@gmail.com>
2017-04-20 16:12:03 -0400

    method for fizzbuzz plus

---
+++ FizzBuzzPlus/FizzBuzzPlus.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzPlus
+{
+    class FizzBuzzPlus
+    {
+        public static string GetFizzBuzz(int x)
+
+        {
+            string characters;
+
+            if (x % 3 == 0 && x % 5 == 0)
+                return "FizzBuzz";
+
+            else if (x % 3 == 0)
+                return "Fizz";
+
+            else if (x % 5 == 0  && x != 35)
+                return "Buzz";
+            else
+                characters = x.ToString();
+            char[] letters = characters.ToCharArray();
+                foreach (char letter in letters)
+                {
+                if (characters == "53" || characters == "35")
+                //if (letter == '5' && letter == '3')
+                {
+                    return "FizzBuzz";
+                }
+
+                else if (letter == '3')
+                {
+                    return "Fizz";
+
+                }
+                else if (letter == '5')
+                {
+                    return "Buzz";
+                }
+                }
+
+
+            return characters;
+
+
+        }
+
+
+
+    }
+
+        }
+
+    
---
+++ FizzBuzzPlus/UnitTest1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzPlus
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string Expected = "1";
+            Assert.AreEqual(Expected, FizzBuzzPlus.GetFizzBuzz(1));
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string Expected = "2";
+            Assert.AreEqual(Expected, FizzBuzzPlus.GetFizzBuzz(2));
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string Expected = "Fizz";
+            Assert.AreEqual(Expected, FizzBuzzPlus.GetFizzBuzz(3));
+
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string Expected = "4";
+            Assert.AreEqual(Expected, FizzBuzzPlus.GetFizzBuzz(4));
+
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string Expected = "Buzz";
+            Assert.AreEqual(Expected, FizzBuzzPlus.GetFizzBuzz(5));
+
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            string Expected = "Fizz";
+            Assert.AreEqual(Expected, FizzBuzzPlus.GetFizzBuzz(6));
+
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            string Expected = "Fizz";
+            Assert.AreEqual(Expected, FizzBuzzPlus.GetFizzBuzz(9));
+
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            string Expected = "Buzz";
+            Assert.AreEqual(Expected, FizzBuzzPlus.GetFizzBuzz(10));
+
+        }
+        [TestMethod]
+        public void TestMethod15()
+        {
+            string Expected = "FizzBuzz";
+            Assert.AreEqual(Expected, FizzBuzzPlus.GetFizzBuzz(15));
+
+        }
+        [TestMethod]
+        public void TestMethod13()
+        {
+            string Expected = "Fizz";
+            Assert.AreEqual(Expected, FizzBuzzPlus.GetFizzBuzz(13));
+
+        }
+
+        [TestMethod]
+        public void TestMethod52()
+        {
+            string Expected = "Buzz";
+            Assert.AreEqual(Expected, FizzBuzzPlus.GetFizzBuzz(52));
+
+        }
+
+        [TestMethod]
+        public void TestMethod53()
+        {
+            string Expected = "FizzBuzz";
+            Assert.AreEqual(Expected, FizzBuzzPlus.GetFizzBuzz(53));
+
+        }
+        [TestMethod]
+        public void TestMethod35()
+        {
+            string Expected = "FizzBuzz";
+            Assert.AreEqual(Expected, FizzBuzzPlus.GetFizzBuzz(35));
+
+        }
+
+
+    }
+}
